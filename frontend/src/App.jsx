0aadfd583d36591e6bb8b2c0abcecbcfac984cf5
kagi <hiroshi_kagiyama_aa@mail.toyota.co.jp>
2024-11-09 14:34:45 +0900

    file changed: App.jsx内をHome.jsxファイルへコンポーネント化

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,8 @@
 import './App.css';
-import { useState } from 'react';
+import Home from './Home.jsx';
 
 function App() {
-  const [userData, setUserData] = useState('');
-  const [inputName, setInputName] = useState('');
-
-  async function handleFetch() {
-    let response = await fetch(`http://localhost:3000/api/users/${inputName}`);
-    response = await response.json();
-    setUserData(response.data);
-  }
-
-  return (
-    <>
-      <h1>My Typing Dojo</h1>
-      <input onChange={(e) => setInputName(e.target.value)} value={inputName} />
-      <br />
-      <button onClick={handleFetch}>fetch</button>
-      <p>{userData.name}</p>
-    </>
-  );
+  return <Home />;
 }
 
 export default App;
---
+++ frontend/src/Home.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+const Home = () => {
+  const [userData, setUserData] = useState('');
+  const [inputName, setInputName] = useState('');
+
+  async function handleFetch() {
+    let response = await fetch(`http://localhost:3000/api/users/${inputName}`);
+    response = await response.json();
+    setUserData(response.data);
+  }
+  return (
+    <>
+      <h1>My Typing Dojo</h1>
+      <input onChange={(e) => setInputName(e.target.value)} value={inputName} />
+      <br />
+      <button onClick={handleFetch}>fetch</button>
+      <p>{userData.name}</p>
+    </>
+  );
+};
+
+export default Home;
