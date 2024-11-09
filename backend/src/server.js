076b826473d717a712236ac1e5eda6282ef3ce26
kagi <hiroshi_kagiyama_aa@mail.toyota.co.jp>
2024-11-09 11:29:52 +0900

    データベースとの接続 cors設定したが、local5173からアクセルできるかあとで確認が必要

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,22 @@
 const userController = require('./user/user.controller');
-
+const cors = require('cors');
 const express = require('express');
 
 function setupServer() {
   const app = express();
-  app.use(express.json());
+  // cors許可の設定 参考：https://zenn.dev/luvmini511/articles/d8b2322e95ff40
+  app.use(
+    cors({
+      origin: 'http://localhost:5173', //アクセス許可するオリジン
+      credentials: true, //レスポンスヘッダーにAccess-Control-Allow-Credentials追加
+      optionsSuccessStatus: 200, //レスポンスstatusを200に設定
+    })
+  );
 
+  app.use(express.json());
+  console.log('response!');
+
+  app.get('/api/users/:id', userController.index);
   app.get('/api/users', userController.view);
 
   return app;
