13b45594bc4b9d32277e3821366970e7ebb458e5
kagi <hiroshi_kagiyama_aa@mail.toyota.co.jp>
2024-11-09 22:34:14 +0900

    機能追加: Shiftとの組み合わせる文字の判定追加 ( や大文字など

--- frontend/src/Typing.jsx
+++ frontend/src/Typing.jsx
@@ -4,6 +4,7 @@ const Typing = ({ sentenceData }) => {
   const [count, setCount] = useState(0);
   const [pressedKeys, setPressedKeys] = useState([]);
   const [isPlay, setIsPlay] = useState(false);
+  const [isShiftPressed, setIsShiftPressed] = useState(false);
 
   function handlePlayStart() {
     if (!isPlay) setIsPlay(true);
@@ -17,25 +18,25 @@ const Typing = ({ sentenceData }) => {
     }
   }
 
-  function handlePressKey(e) {
+  function handleKeyDown(e) {
     let newPressedKeys;
     if (e.key === 'Backspace') {
       const tempPressedKeys = [...pressedKeys];
       tempPressedKeys.pop();
       newPressedKeys = [...tempPressedKeys];
     } else {
-      const pressedKey = e.key === 'Shift' ? ' ' : e.key;
+      const pressedKey = e.key === 'Shift' ? '' : e.key;
       newPressedKeys = [...pressedKeys, pressedKey];
     }
     const joinNewPressKeys = newPressedKeys.join('');
-    console.log(joinNewPressKeys);
     setPressedKeys(newPressedKeys);
   }
 
   return (
-    <div onKeyDown={(e) => handlePressKey(e)} tabIndex="0">
+    <div onKeyDown={(e) => handleKeyDown(e)} tabIndex="0">
       <div>Typing display!</div>
       <p>{sentenceData[count].sentence}</p>
+      <p>{pressedKeys.join('')}</p>
       <button onClick={handlePlayStart}>start</button>
       <button onClick={handleNextClick}>next</button>
     </div>
