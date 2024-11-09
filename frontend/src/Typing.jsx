2082ed1799256ab18df59e5a421f228023656cee
kagi <hiroshi_kagiyama_aa@mail.toyota.co.jp>
2024-11-09 20:06:01 +0900

    機能追加: 入力文字を連結する処理を追加、Shiftを半角スペースに変換、BackSpaseで削除

--- frontend/src/Typing.jsx
+++ frontend/src/Typing.jsx
@@ -1,10 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 
 const Typing = ({ sentenceData }) => {
   const [count, setCount] = useState(0);
   const [pressedKeys, setPressedKeys] = useState([]);
+  const [isPlay, setIsPlay] = useState(false);
+
+  function handlePlayStart() {
+    if (!isPlay) setIsPlay(true);
+  }
 
   function handleNextClick() {
+    if (!isPlay) setIsPlay(true);
     // countの上限制御
     if (sentenceData.length - 1 > count) {
       setCount((count) => count + 1);
@@ -12,15 +18,25 @@ const Typing = ({ sentenceData }) => {
   }
 
   function handlePressKey(e) {
-    const newPressedKeys = [...pressedKeys, e.key];
+    let newPressedKeys;
+    if (e.key === 'Backspace') {
+      const tempPressedKeys = [...pressedKeys];
+      tempPressedKeys.pop();
+      newPressedKeys = [...tempPressedKeys];
+    } else {
+      const pressedKey = e.key === 'Shift' ? ' ' : e.key;
+      newPressedKeys = [...pressedKeys, pressedKey];
+    }
+    const joinNewPressKeys = newPressedKeys.join('');
+    console.log(joinNewPressKeys);
     setPressedKeys(newPressedKeys);
   }
-  console.log('pressedKeys: ', pressedKeys);
 
   return (
     <div onKeyDown={(e) => handlePressKey(e)} tabIndex="0">
       <div>Typing display!</div>
       <p>{sentenceData[count].sentence}</p>
+      <button onClick={handlePlayStart}>start</button>
       <button onClick={handleNextClick}>next</button>
     </div>
   );
