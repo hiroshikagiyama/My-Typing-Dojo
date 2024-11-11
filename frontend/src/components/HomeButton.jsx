32ba61976da8fb3f77be68df7533d12dae8b0448
kagi <hiroshi_kagiyama_aa@mail.toyota.co.jp>
2024-11-11 23:44:47 +0900

    スタイリング変更: buttonフォントサイズアップ

--- frontend/src/components/HomeButton.jsx
+++ frontend/src/components/HomeButton.jsx
@@ -3,7 +3,14 @@ import { Button } from '@chakra-ui/react';
 
 export const HomeButton = ({ children, handleFetch }) => {
   return (
-    <Button mt="4" variant="subtle" px="8" mx="4" onClick={handleFetch}>
+    <Button
+      textStyle="md"
+      mt="4"
+      variant="subtle"
+      px="8"
+      mx="4"
+      onClick={handleFetch}
+    >
       {children}
     </Button>
   );
--- frontend/src/components/TypingButton.jsx
+++ frontend/src/components/TypingButton.jsx
@@ -3,7 +3,14 @@ import { Button } from '@chakra-ui/react';
 
 export const TypingButton = ({ children, clickFunc }) => {
   return (
-    <Button variant="subtle" px="8" mx="4" mt="20" onClick={clickFunc}>
+    <Button
+      textStyle="md"
+      variant="subtle"
+      px="8"
+      mx="4"
+      mt="20"
+      onClick={clickFunc}
+    >
       {children}
     </Button>
   );
