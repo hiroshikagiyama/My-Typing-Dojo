c0d94f81f0bb743a9b8205dc1d813a22f7781d87
kagi <hiroshi_kagiyama_aa@mail.toyota.co.jp>
2024-11-13 09:54:51 +0900

    画面追加：ログイン画面 chakra uiのpassword componentsを利用

--- frontend/src/Main.jsx
+++ frontend/src/Main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client';
 import { Provider } from './components/ui/provider';
 import './index.css';
 import Login from './Login.jsx';
-import { Center } from '@chakra-ui/react';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
