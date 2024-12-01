c7a0e849508c7fdbdb7221804bddccda4fb9edb7
kagi <hiroshi_kagiyama_aa@mail.toyota.co.jp>
2024-12-01 20:39:06 +0900

    salt列の削除

--- backend/db/migrations/20241108143755_create-user-list-table.js
+++ backend/db/migrations/20241108143755_create-user-list-table.js
@@ -2,7 +2,6 @@ exports.up = async (knex) => {
   await knex.schema.createTable('user_list', (table) => {
     table.increments('id').primary();
     table.string('username').notNullable();
-    table.string('salt').notNullable();
     table.string('hashed_password').notNullable();
   });
 };
